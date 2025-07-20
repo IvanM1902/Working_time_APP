90fc33fd9c265375602a6f7e13d11756de961d87
IvanM1902 <ivan.medved.1902@gmail.com>
2025-07-20 18:08:32 +0100

    setting up springboot

---
+++ backend/src/main/java/com/example/workingtimeapp/WorkingtimeappApplication.java
@@ -0,0 +1,13 @@
+package com.example.workingtimeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkingtimeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkingtimeappApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/workingtimeapp/WorkingtimeappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workingtimeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkingtimeappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
